4569e2568c448df57d8ea1062a2f37c1f109ded9
nmartovs <nmartovs@evolutiongaming.com>
2020-09-24 18:10:06 +0300

    Initial

---
+++ project/src/Basics.scala
@@ -0,0 +1,19 @@
+class Basics extends App {
+  def gcd(a: Int, b: Int, divider: Int = 2): Int = {
+    val lo = scala.math.min(a, b)
+    val hi = scala.math.max(a, b)
+    if (divider <= lo) {
+      if (lo % divider == 0 && hi % divider == 0) {
+        divider * gcd(lo / divider, hi / divider, divider)
+      } else {
+        gcd(lo, hi, divider + 1)
+      }
+    } else {
+      1
+    }
+  }
+
+  def lcm(a: Int, b:Int): Int = {
+    a * b / gcd(a, b)
+  }
+}
